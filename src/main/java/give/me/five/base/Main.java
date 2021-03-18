d04b101bf395d32b5f03711f7cb38a6cffa428f2
liziyan13 <liziyan13@jd.com>
2021-03-18 15:39:14 +0800

    20210318

--- src/main/java/give/me/five/base/Main.java
+++ src/main/java/give/me/five/base/Main.java
@@ -1,8 +1,19 @@
 package give.me.five.base;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello, lzy");
+        System.out.println("1、java中的基本数据类型类型有哪8种:int,long,char,float,double,boolean\nbyte,short这两个忘记了\n");
+        System.out.println("2、实现一个类，提供四个方法，分别实现int类型的加、减、乘、除运算。");
+        TestArithmetic m = new TestArithmetic();
+        Scanner sc=new Scanner(System.in);
+        System.out.print("请输入第一个操作数:");
+        float firstNum = sc.nextInt();
+        System.out.print("请输入第二个操作数:");
+        float secondNum = sc.nextInt();
+        System.out.println("a+b="+m.add(firstNum,secondNum)+"\na-b="+m.sub(firstNum,secondNum)+"\na*b="+m.mul(firstNum,secondNum)+"\na/b="+m.div(firstNum,secondNum));
     }
 }
--- src/main/java/give/me/five/solution/s1/README.md
+++ src/main/java/give/me/five/solution/s1/README.md
@@ -1,5 +1,8 @@
 #### 问题描述
 
 1、java中的基本数据类型类型有哪8种？
+int,long,char,float,double,boolean;byte,short
+byte（字节型）这个忘记了、short（短整型）、int（整型）、long（长整型）、float（单精度浮点型）、double（双精度浮点型）、boolean（布尔型）、char（字符型）
+
 
 2、实现一个类，提供四个方法，分别实现int类型的加、减、乘、除运算。
