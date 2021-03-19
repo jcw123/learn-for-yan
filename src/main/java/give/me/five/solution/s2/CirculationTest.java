1062fb5cb082684f0d3c01c6701bf9af0eb29ad6
liziyan13 <liziyan13@jd.com>
2021-03-19 15:20:53 +0800

    最终版20210319

--- src/main/java/give/me/five/solution/s2/CirculationTest.java
+++ src/main/java/give/me/five/solution/s2/CirculationTest.java
@@ -23,6 +23,7 @@ public class CirculationTest {
         }
     }
 
+
     public static void main(String[] args) throws Exception {
         Scanner sc=new Scanner(System.in);
         System.out.print("请输入一个操作数:");
